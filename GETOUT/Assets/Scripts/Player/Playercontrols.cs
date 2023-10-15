c3a4ea54999671dfe24fc804371f1677e4fd4d35
Duke0fCodes <137860801+Duke0fCodes@users.noreply.github.com>
2023-10-15 18:13:05 +0530

    changes

--- GETOUT/Assets/Scripts/Player/Playercontrols.cs
+++ GETOUT/Assets/Scripts/Player/Playercontrols.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public LayerMask solidobjectslayer;
+    public LayerMask interactablelayer;
     public LayerMask wallsLayer;
     private bool isMoving;
     private Vector2 input;
@@ -38,6 +39,19 @@ public class PlayerController : MonoBehaviour
         }
         
         animator.SetBool("isMoving",isMoving);
+        if(Input.GetKeyDown(KeyCode.Z))
+            Interact();
+    }
+    void Interact()
+    {
+        var facingDir= new Vector3(animator.GetFloat("moveX"),animator.GetFloat("moveY"));
+        var interactPos= transform.position + facingDir;
+        //Debug.DrawLine(transform.position, interactPos, Color.green, 0.5f);
+        var collider=Physics2D.OverlapCircle(interactPos, 0.3f, interactablelayer);
+        if(collider != null)
+        {
+            collider.GetComponent<Interactable>()?.Interact();
+        }
     }
     IEnumerator Move(Vector3 targetPos)
     {
@@ -53,7 +67,7 @@ public class PlayerController : MonoBehaviour
     }
     private bool IsWalkable(Vector3 targetPos)
         {
-            if(Physics2D.OverlapCircle(targetPos, 0.2f, solidobjectslayer)!=null)
+            if(Physics2D.OverlapCircle(targetPos, 0.2f, solidobjectslayer | interactablelayer)!=null)
             {
                 return false;
             }
